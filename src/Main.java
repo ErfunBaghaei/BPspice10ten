b4d21534ec4ac26b1a65ee87fcb5ae2eb2e81a77
apourghani <apourghani@gmail.com>
2020-07-03 18:35:07 +0430

    test1

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("by");
         System.out.println("hierfun");
         System.out.println("hierfun");
-       System.o
+       System.out.println("hihihihihi");
 
 
     }
