1d357f66645c80a5a3858a7cba51927a0d5f1b26
ErfunBaghaei <66559487+ErfunBaghaei@users.noreply.github.com>
2020-07-03 18:20:56 +0430

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("yesss ذشذغ");
+        System.out.println("giiiiiiit");
+        System.out.println("salammmmm");
+        System.out.println("by");
+        System.out.println("hierfun");
+        System.out.println("hierfun");
+       System.o
+
+
+    }
+
+}
