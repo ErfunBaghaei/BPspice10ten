28eef798a80232d4a20dc73ec6c2f907f4c1738e
apourghani <apourghani@gmail.com>
2020-08-04 00:32:42 +0430

    project is 99percent finished

--- src/VoltageControledCurrentSource.java
+++ src/VoltageControledCurrentSource.java
@@ -1,7 +1,5 @@
 public class VoltageControledCurrentSource extends Element {
     String input;
-    double gain = 0;
-
     public VoltageControledCurrentSource(String input) {
         super.type = "vcc";
         this.input = input;
